c2c991bea9878766a051831b092edc441aaac2f9
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-02-18 11:03:19 -0600

    Remove some dead code

--- dimsim/datasets/thermoml/__init__.py
+++ dimsim/datasets/thermoml/__init__.py
@@ -1,11 +1,3 @@
-from dimsim.datasets.thermoml.plugins import (
-    register_thermoml_property,
-    thermoml_property,
-)
 from dimsim.datasets.thermoml.thermoml import ThermoMLDataSet
 
-__all__ = (
-    "ThermoMLDataSet",
-    "register_thermoml_property",
-    "thermoml_property",
-)
+__all__ = ("ThermoMLDataSet",)
