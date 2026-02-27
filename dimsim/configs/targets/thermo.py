cf26732ae10b480e1ba9532ba5caf7bb2b1ef622
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-02-27 14:20:57 -0600

    Start adding data schema

--- dimsim/configs/targets/thermo.py
+++ dimsim/configs/targets/thermo.py
@@ -1,17 +1,43 @@
 import typing
 
 from dimsim.datasets.datasets import PropertyPhase
+from dimsim.molecule import map_smiles
+import pyarrow
+import datasets
 
 EntryTag = typing.Literal["density"]
 
 """
-['Mass density, kg/m3',
- 'Excess molar volume, m3/mol',
- 'Relative permittivity at zero frequency',
- 'Excess molar enthalpy (molar enthalpy of mixing), kJ/mol',
- 'Molar enthalpy of vaporization or sublimation, kJ/mol']
-"""
 
+DATA_SCHEMA = pyarrow.schema(
+    [
+        ("type", pyarrow.string()),
+        ("smiles_a", pyarrow.string()),
+        ("x_a", pyarrow.float64()),
+        ("smiles_b", pyarrow.string()),
+        ("x_b", pyarrow.float64()),
+        ("temperature", pyarrow.float64()),
+        ("pressure", pyarrow.float64()),
+        ("value", pyarrow.float64()),
+        ("std", pyarrow.float64()),
+        ("units", pyarrow.string()),
+        ("source", pyarrow.string()),
+    ]
+)
+"""
+DATA_SCHEMA = pyarrow.schema(
+    [
+        ("phases", pyarrow.list_(pyarrow.string())),
+        ("smiles", pyarrow.list_(pyarrow.string())),
+        ("x", pyarrow.list_(pyarrow.float64())),
+        ("temperature", pyarrow.float64()),
+        ("pressure", pyarrow.float64()),
+        ("value", pyarrow.float64()),
+        ("std", pyarrow.float64()),
+        ("units", pyarrow.string()),
+        ("source", pyarrow.string()),
+    ]
+)
 
 class DataEntry(typing.TypedDict):
     phases: list[PropertyPhase]
@@ -55,3 +81,13 @@ class EnthalpyOfVaporizationEntry(DataEntry):
 
 class VaporPressureEntry(DataEntry):
     phases: list[PropertyPhase] = PropertyPhase.Liquid | PropertyPhase.Gas
+
+
+def create_dataset(*rows: DataEntry) -> datasets.Dataset:
+    for row in rows:
+        row["smiles"] = [map_smiles(value) for value in row["smiles"]]
+
+    # TODO: validate rows
+    table = pyarrow.Table.from_pylist([*rows], schema=DATA_SCHEMA)
+
+    return datasets.Dataset(datasets.table.InMemoryTable(table))
