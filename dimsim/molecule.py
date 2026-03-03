ad6461c37561b1cca2231efd33e205b12ae02aa3
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-03-03 11:29:45 -0600

    Add file

---
+++ dimsim/molecule.py
@@ -0,0 +1,7 @@
+from openff.toolkit import Molecule
+
+def map_smiles(smiles: str) -> str:
+    molecule: Molecule = Molecule.from_smiles(smiles)
+    mapped_smiles: str = molecule.to_smiles(mapped=True)  # type: ignore[return-value]
+
+    return mapped_smiles
