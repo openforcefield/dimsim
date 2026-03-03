6d23b88de4e23bc378fb0d665919dedd132fb74c
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-03-03 11:25:54 -0600

    Lint

--- dimsim/configs/targets/thermo.py
+++ dimsim/configs/targets/thermo.py
@@ -1,9 +1,10 @@
 import typing
 
+import datasets
+import pyarrow
+
 from dimsim.datasets.datasets import PropertyPhase
 from dimsim.molecule import map_smiles
-import pyarrow
-import datasets
 
 EntryTag = typing.Literal["density"]
 
@@ -38,6 +39,7 @@ DATA_SCHEMA = pyarrow.schema(
         ("source", pyarrow.string()),
     ]
 )
+
 
 class DataEntry(typing.TypedDict):
     phases: list[PropertyPhase]
