ee3b82486730371542e66256fe223e8a53f37d34
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-08-04 12:47:48 -0500

    Fixes

--- dimsim/configs/gas.py
+++ dimsim/configs/gas.py
@@ -1,7 +1,6 @@
 import typing
 
 from dimsim.configs._compute import BaseComputeConfig
-from dimsim.configs.targets.thermo import DataEntry
 
 
 class VacuumGas(BaseComputeConfig):
@@ -16,23 +15,3 @@ class VacuumGas(BaseComputeConfig):
 
     units: str
     """
-
-
-def gas_config_from_data_entry(
-    data_entry: DataEntry,
-    force_field: str,
-    n_molecules: int,
-) -> VacuumGas:
-    """
-    Create a `VacuumGas` config from thermophysical and chemical information in a `DataEntry`
-    and job-specific inputs.
-    """
-    return VacuumGas(  # type: ignore[typeddict-unknown-key]
-        tag="gas",
-        force_field=force_field,
-        n_molecules=n_molecules,
-        smiles=data_entry["smiles"],
-        x=data_entry["x"],
-        temperature=data_entry["temperature"],
-        pressure=data_entry["pressure"],
-    )
--- dimsim/configs/liquid.py
+++ dimsim/configs/liquid.py
@@ -1,7 +1,6 @@
 import typing
 
 from dimsim.configs._compute import BaseComputeConfig
-from dimsim.configs.targets.thermo import DataEntry
 
 
 class BulkLiquid(BaseComputeConfig):
@@ -20,24 +19,3 @@ class BulkLiquid(BaseComputeConfig):
 
     units: str
     """
-
-
-def liquid_config_from_data_entry(
-    data_entry: DataEntry,
-    force_field: str,
-    n_molecules: int,
-) -> BulkLiquid:
-    """
-    Create a `BulkLiquid` config from thermophysical and chemical information in a `DataEntry`
-    and job-specific inputs.
-    """
-    return BulkLiquid(
-        tag="liquid",
-        force_field=force_field,
-        n_molecules=n_molecules,
-        smiles=data_entry["smiles"],
-        x=data_entry["x"],
-        temperature=data_entry["temperature"],
-        pressure=data_entry["pressure"],
-        density=data_entry["value"],
-    )
